a0a8d5dd5e982e5af486be95de6ef3578b66fe7d
nantman-commits <kai.nantaman@gmail.com>
2026-08-27 11:17:32 -0700

    my array list

--- Unit 1/PugRescue/PugSaver.java
+++ Unit 1/PugRescue/PugSaver.java
@@ -4,17 +4,12 @@ public class PugSaver {
 
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
-		// right now, just takes last without check
 		int lastNonPugIndex = list.size() - 1;
 		for (int nextIndex = 0; nextIndex < list.size(); nextIndex++) {
-			// if (nextIndex >= lastNonPugIndex) {
-			// 	return;
-			// }
 			Dog d = list.get(nextIndex);
 			if (!d.getBreed().equals("Pug")) {
 				continue;
 			}
-			// find next non pug, which starts off being a pug since swapped to there
 			Dog lastNonPug = list.get(lastNonPugIndex);
 			while (lastNonPug.getBreed().equals("Pug")) {
 				lastNonPugIndex--;
